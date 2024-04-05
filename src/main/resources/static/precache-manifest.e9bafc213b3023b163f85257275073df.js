3d4c32b1a9ad58c558d5cb2c42ce890e0ee765ae
Tejaswinibn <tej04041999@gmail.com>
2024-04-04 23:02:15 -0400

    Static

---
+++ src/main/resources/static/precache-manifest.e9bafc213b3023b163f85257275073df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7e51cbb395aeb19bab4ae28bf05be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eff10bad8c89cd92279",
+    "url": "/static/css/2.48d95ff0.chunk.css"
+  },
+  {
+    "revision": "b669d1b170db3b242e8f",
+    "url": "/static/css/main.63474304.chunk.css"
+  },
+  {
+    "revision": "9eff10bad8c89cd92279",
+    "url": "/static/js/2.ff7072c0.chunk.js"
+  },
+  {
+    "revision": "b669d1b170db3b242e8f",
+    "url": "/static/js/main.e2068c1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
